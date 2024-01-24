83161a289cb656c46a0155892f667ff46422738f
Yesid Gonzalez <sldeiby06@gmail.com>
2024-01-24 12:20:17 -0500

    delete old elements

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from controllers.pricesController import pricesRouter
-from controllers.earningsController import earningsRouter
-
-app = FastAPI()
-
-app.include_router(pricesRouter)
-app.include_router(earningsRouter)
